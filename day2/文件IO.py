05557d1d09ed3cdaf0b0981f49945fb090710d9b
jingwenshuai <18911222403>
2019-05-12 13:46:24 +0800

    添加了一些基础自动化语法和练习

--- day2/文件IO.py
+++ day2/文件IO.py
@@ -29,21 +29,57 @@
 
 #------------------------------------------#
 #练习：
+#--------read()方法读出来的是一个str类型
+#--------readlines()方法读出来的是一个list类型
 
-with open('C:\D\VIPtest2\data.txt') as f:
+# with open('C:\D\VIPtest2\data.txt') as f:
+#     res = f.read()
+#     print(res)
+#     print(type(res))
+#     f.seek(0)
+#     oo = f.readlines()
+#     print('------------')
+#     print(oo)
+#     print(type(oo))
+#     m = res.replace("\n",",")
+#     L = []
+#     x = res.split(',')
+#     # print (x)
+#     for a in oo:
+#         if a.isdigit():
+#             L.append(a)
+#         f.close()
+#         L.reverse()
+#     print(L)
+'''
+with open('C:\D\VIPtest2\data.txt','r+') as f:
+    print(f.read())
+    print(type(f.read()))
+    print('----------------------------------')
+    f.seek(0)
+    print(f.readlines())
+    print(type(f.readlines()))
+    print('----------------------------------')
+    f.seek(0)
+    print(f.readline())
+    print(type(f.readline()))
+'''
+
+with open('C:\D\VIPtest2\data.txt','r+') as f:
     res = f.read()
-    m = res.replace("\n",",")
-    L = []
-    x = m.split(' ')
-    print (x)
-    for i in x:
-        for a in i:
-            print(a)
-            if a.isdigit():
-                L.append(a)
-            else:
-                continue
-            f.close()
-        L.reverse()
-        print()
+    print(res)
 
+    #字符串替换，将空格替换为逗号
+    res = res.replace('\n',',')
+    print('res为：',res)
+
+    #字符串分割，将字符串以逗号分隔成为一个列表
+    m = res.split(',')
+    print('m为：',m)
+    P = []
+    for i in m:
+        if i.isdigit():
+            P.append(i)
+    #sort()排序方法直接改变列表
+    P.sort()
+    print(P)
