feb1ce553a3c5ec90cd15546bacfaf9b309a45c3
jingwenshuai <18911222403>
2019-05-13 22:19:19 +0800

    Python基础语法

--- day2/练习5_12.py
+++ day2/练习5_12.py
@@ -4,17 +4,3 @@
 # with open('C:\D\VIPtest2\day2\data.txt','r+') as f:
 #     for line in f.readlines():
 #         print(line.strip())
-
-def bubble(bubbleList):
-    listLength = len(bubbleList)
-    while listLength > 0:
-        for i in range(listLength - 1):
-            if bubbleList[i] > bubbleList[i + 1]:
-                bubbleList[i], bubblelist[i + 1] = bubblelist[i + 1], bubblelist[i]
-        listLength -= 1
-    print(bubbleList)
-
-
-if __name__ == '__main__':
-    li = [3, 4, 1, 2, 5, 8, 0]
-    bubble(li)
